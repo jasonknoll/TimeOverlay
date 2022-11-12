d01327162487ffd35368151173d30ae2a2cf9cc2
Jason Knoll <jasonknoll98@aol.com>
2022-11-12 14:04:02 -0500

    Moved location to top right. Added 'ctrl+shift+Q' shortcut to quit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,20 @@
 import java.awt.Component
 import java.awt.Dimension
 import java.awt.Font
+import java.awt.Toolkit
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
 import javax.swing.JFrame
 import javax.swing.JTextArea
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import javax.swing.JLabel
+import kotlin.system.exitProcess
 
 // Maybe scale based on font
 // This is good for Monaco 20
-const val WIDTH = 100
-const val HEIGHT = 80
+const val WIDTH = 120
+const val HEIGHT = 60
 
 /**
  *
@@ -21,29 +26,142 @@ const val HEIGHT = 80
  *
  * */
 
+class ClockThread(runFlag: Boolean, textArea: JTextArea, formatter: DateTimeFormatter):Runnable {
+    var _flag: Boolean = false
+    var _formatter = formatter
+    var _textArea = textArea
+
+    init {
+        this._flag = runFlag
+    }
+
+    override fun run() {
+        while(_flag) {
+            val current = LocalDateTime.now()
+            val formattedTime = current.format(_formatter)
+            _textArea.text = formattedTime
+        }
+    }
+}
+
+class JKKeyListener(frame: JFrame): KeyListener {
+    var ctrlPressed = false
+    var shiftPressed = false
+    private var frame: JFrame
+    init {
+        this.frame = frame
+    }
+    override fun keyTyped(e: KeyEvent) {
+
+    }
+
+    override fun keyPressed(e: KeyEvent?) {
+
+        if (e != null) {
+            when (e.keyCode)  {
+                KeyEvent.VK_CONTROL -> this.ctrlPressed = true
+                KeyEvent.VK_SHIFT -> this.shiftPressed = true
+                KeyEvent.VK_Q ->  {
+                    if (this.shiftPressed && this.ctrlPressed) {
+                        frame.dispose()
+                        exitProcess(0)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun keyReleased(e: KeyEvent?) {
+        if (e != null) {
+            when (e.keyCode)  {
+                KeyEvent.VK_CONTROL -> this.ctrlPressed = false
+                KeyEvent.VK_SHIFT -> this.shiftPressed = false
+            }
+        }
+    }
+}
+
+class testSecondThread:Runnable{
+
+    private fun createNewWindow() {
+        val jl = JLabel("THis is a new window!!!")
+
+        val frame = JFrame("Test Frame")
+        frame.add(jl)
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.size = Dimension(200, 200)
+
+        frame.isVisible = true
+    }
+
+    init {
+        val jl = JLabel("test text")
+
+        val frame = JFrame("Test Frame")
+        frame.add(jl)
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.size = Dimension(200, 200)
+
+        frame.isVisible = true
+    }
+    override fun run() {
+
+    }
+}
+
+fun testThread() {
+    val secondThread = testSecondThread()
+    val thread = Thread(secondThread)
+
+    thread.start()
+}
+
+
+fun updateClock(textArea: JTextArea, formatter: DateTimeFormatter, flag: Boolean) {
+    val clockThread = ClockThread(flag, textArea, formatter)
+    val thread = Thread(clockThread)
+
+    thread.start()
+    thread.join()
+}
+
+
 fun main(args: Array<String>) {
 
+    // Setup
     val textArea = JTextArea()
 
     val formatter = DateTimeFormatter.ofPattern("HH:mm:ss\nMM-dd-yyyy")
-
-    val current = LocalDateTime.now()
-    val formattedTime = current.format(formatter)
-
-    textArea.text = formattedTime
 
     //textArea.size = Dimension()
     textArea.font = Font("Monaco", Font.PLAIN, 20)
     //textArea.setBounds()
 
+    // Don't do anything??
     textArea.alignmentX = Component.CENTER_ALIGNMENT
     textArea.alignmentY = Component.CENTER_ALIGNMENT
 
 
     val frame = JFrame("Date and Time Overlay")
     frame.add(textArea)
+    textArea.addKeyListener(JKKeyListener(frame))
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     frame.size = Dimension(WIDTH, HEIGHT)
 
+    frame.isUndecorated = true
+    frame.isAlwaysOnTop = true
+
+    val screenSize = Toolkit.getDefaultToolkit().screenSize
+
+
+    val x = screenSize.getWidth()
+    //val y = screenSize.getHeight().toInt()
+
+    frame.setLocation((x.toFloat() * 0.75).toInt(), 0)
+
+
     frame.isVisible = true
+
+    val isRunning = true
+    updateClock(textArea, formatter, isRunning)
 }
